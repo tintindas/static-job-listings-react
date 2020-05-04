acdf087c02c63d22b58ce94d3087568086801bc4
Upamanyu Das <upamanyu.das.work@gmail.com>
2020-05-04 22:26:30 +0530

    Updates

---
+++ precache-manifest.9e545827922c3860556257939c1f33b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2e307ee222f2d6139784d7a2e79785",
+    "url": "/static-job-listings-react/index.html"
+  },
+  {
+    "revision": "2a66462a472851eb2db1",
+    "url": "/static-job-listings-react/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0f88ff246e921a607ac5",
+    "url": "/static-job-listings-react/static/js/2.24ae2283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static-job-listings-react/static/js/2.24ae2283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a66462a472851eb2db1",
+    "url": "/static-job-listings-react/static/js/main.a23042fd.chunk.js"
+  },
+  {
+    "revision": "8a18c5a378b9a22202dc",
+    "url": "/static-job-listings-react/static/js/runtime-main.df9e0e38.js"
+  }
+]);
